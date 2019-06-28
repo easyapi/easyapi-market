743a945a38e10886cc1a42558f2ff3ff0f4f9dbf
胡亚飞 <hyf@bangqu.com>
2019-06-28 15:10:44 +0800

    更新

--- plugins/axios.js
+++ plugins/axios.js
@@ -7,7 +7,7 @@ axios.defaults.baseURL = 'https://api2.easyapi.com'
 
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
-  // Cookies.set('authenticationToken','eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoeWZAYmFuZ3F1LmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE1NjI0MTgzOTN9.M64dJH7n4wrMgqpv4ABgGeAjnM-3eeijl_NNBa4GeHXRqnkuY8z2ErW30Cm6TZCgg07CXJWwNCsoJDm6FdX6hA')
+  // Cookies.set('authenticationToken','eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6aGFuZzIwMDg0QDEyNi5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTiIsImV4cCI6MTU2MzY5NTI4Mn0.ZovWMECa9eo3wcNRTxkYZUR_BnB8BlDmZVUCQIIBhyiFzbp-2XtY9o6ou3-t-e1UqXFLPtSiWHnFGVDnDTj49A')
   // 全局加Token
   config.headers.Authorization = 'Bearer ' + Cookies.get('authenticationToken')
   return config
