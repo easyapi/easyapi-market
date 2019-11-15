5132758295377c4e1c8372d9fe8186e283d753ad
leida <zhang20084@126.com>
2019-11-16 02:12:03 +0800

    代码优化

--- api/api.js
+++ api/api.js
@@ -1,11 +1,15 @@
 // 获取用户信息
 export const getUser = `https://account.easyapi.com/api/account`
+
 // 获取用户团队列表
 export const getMyTeam = `https://account.easyapi.com/api/user/teams`
+
 // 切换团队
 export const changeTeam = `https://account.easyapi.com/api/changeTeam`
 
+
 // 获取服务列表
 export const getServiceList = `/api/services`
+
 // 获取服务类型
 export const getServiceTypeList = `/api/service/types`
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,30 +1,20 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
-// // request header
+
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'https://api2.easyapi.com'
-// axios.defaults.headers.post['encryptDisable'] = true;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-// 添加请求拦截器
 axios.interceptors.request.use(function (config) {
-  // Cookies.set('authenticationToken', 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoeWZAYmFuZ3F1LmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE1NjUyNjg2NzV9.S1lGzJ-W0qJBtDWN_XL8jdw4vhTBKpZnh7woZCYvt7TQrNiUMlOaAEg27lTEnlpPqVLf3NIHu4B18aFEJx3AQQ')
-  let token = Cookies.get('authenticationToken')
-  if (token) {
-    // 全局加Token
+  if (Cookies.get('authenticationToken')) {
     config.headers.Authorization = 'Bearer ' + Cookies.get('authenticationToken')
   }
-
   return config
 }, function (error) {
-  // 对请求错误做些什么
   console.log(error)
   return Promise.reject(error)
 })
 
-// 添加响应拦截器
 axios.interceptors.response.use(function (response) {
-
   return response
 }, function (error) {
   return Promise.reject(error)
