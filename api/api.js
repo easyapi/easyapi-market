6beedcb7c3c13c2c47bfbfb85ffa8537e3a23ce6
leida <zhang20084@126.com>
2020-09-25 08:55:07 +0800

    fix:接口地址修改

--- api/api.js
+++ api/api.js
@@ -1,5 +1,2 @@
-import axios from 'axios'
-
-// export const baseUrl = 'https://account-api.easyapi.com'
-export const baseUrl = 'http://localhost:8080'
+export const baseUrl = 'https://account-api.easyapi.com'
 export const serviceUrl = 'https://api2.easyapi.com'
