a85bc50204b26be027c6b38b830697bb797c0afe
leida <zhang20084@126.com>
2020-09-25 09:00:06 +0800

    fix:用户令牌修改

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,9 +3,7 @@ import Cookies from 'js-cookie'
 export default function ({ $axios, redirect }) {
   $axios.onRequest(config => {
     if (Cookies.get('authenticationToken')) {
-      // config.headers.Authorization = 'Bearer ' + Cookies.get('authenticationToken')
-      config.headers.Authorization = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzU0NTM5MDUyN0BiYW5ncXUuY29tIiwiYXV0aCI6IiIsImV4cCI6MTU4NjU3MTc0MX0.5yLHA2v0oyOjQ1jnMO3Jyb9lspIOn0zQmh3S-dUg0cVIv-SFfjV1LJyRwu8FLMFwFt_wRsZKKHNoj1D0psgcVQ'
-
+      config.headers.Authorization = 'Bearer ' + Cookies.get('authenticationToken')
     }
   })
   $axios.onError(error => {
