a6eace415e0926fa8dfcc70a42842ee94d79bb8a
chenyun123 <1537299563@qq.com>
2021-04-14 19:54:19 +0800

    更多加载效果

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -2,12 +2,20 @@
   <div>
     <Header></Header>
     <div class="container">
-      <div v-for="item of list" :key="item.articleId">
-        <ListItem v-bind:list="item"></ListItem>
+      <div v-if="!noData">
+        <div v-for="item of list" :key="item.articleId">
+          <ListItem v-bind:list="item"></ListItem>
+        </div>
+      </div>
+      <div v-else class="no-data">
+        <img src="../../assets/images/no-data.png" alt="" />
       </div>
       <div class="loading" v-if="loading">
         <Icon type="ios-loading" size="18" class="demo-spin-icon-load"></Icon>
         <div>加载中......</div>
+      </div>
+      <div class="noMoreData" v-if="noMoreData">
+        <div>没有更多数据了...</div>
       </div>
     </div>
     <Footer></Footer>
@@ -40,6 +48,8 @@ export default {
     return {
       list: [],
       loading: false,
+      noData: false,
+      noMoreData: false,
       pagination: {
         size: 15,
         page: 0,
@@ -52,8 +62,9 @@ export default {
       if (this.pagination.page < this.pagination.totalPages - 1) {
         this.pagination.page = this.pagination.page + 1
         this.getArticleList()
-      } else {
-        // this.noMoreData = true
+      }
+      if(this.pagination.page == this.pagination.totalPages - 1){
+        this.noMoreData = true
       }
     },
     getArticleList() {
@@ -68,7 +79,7 @@ export default {
           this.pagination.totalPages = res.data.totalPages
           this.loading = false
         } else {
-          // this.noData = true;
+          this.noData = true
           this.loading = false
         }
       })
@@ -79,7 +90,7 @@ export default {
         document.documentElement.scrollTop || document.body.scrollTop
       let clientHeight = document.documentElement.clientHeight
       let scrollHeight = document.documentElement.scrollHeight
-      if (scrollTop + clientHeight >= scrollHeight) {
+      if (scrollHeight - clientHeight - scrollTop <= 150) {
         // 滚动到底部，逻辑代码
         //事件处理
         this.getPageList()
@@ -90,6 +101,7 @@ export default {
   mounted() {
     window.addEventListener('scroll', this.lazyLoading)
     this.getArticleList()
+     
   },
 }
 </script>
@@ -98,5 +110,13 @@ export default {
 .loading {
   text-align: center;
   color: #2d8cf0;
+}
+.noMoreData {
+  text-align: center;
+  color: #2d8cf0;
+}
+.no-data {
+  margin: 400px auto;
+  text-align: center;
 }
 </style>
