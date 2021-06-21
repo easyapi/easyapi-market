168692380b681ccd342f0805c761986521e1c105
chenyun123 <1537299563@qq.com>
2021-06-21 15:55:09 +0800

    切换团队弹窗移到前面

--- components/header.vue
+++ components/header.vue
@@ -2,22 +2,26 @@
   <div class="header flex-r">
     <ul class="header-con-left flex-r">
       <div class="header-logo flex-r">
-        <nuxt-link :to="{name:'index'}" class="logo flex-r">
-          <img src="https://qiniu.easyapi.com/market/logo.png" alt width="100"/>
+        <nuxt-link :to="{ name: 'index' }" class="logo flex-r">
+          <img
+            src="https://qiniu.easyapi.com/market/logo.png"
+            alt
+            width="100"
+          />
         </nuxt-link>
         <span class="circle"></span>
       </div>
       <li class="header-market">
-        <nuxt-link :to="{name:'index'}">API市场</nuxt-link>
+        <nuxt-link :to="{ name: 'index' }">API市场</nuxt-link>
       </li>
       <li class="item-menu mg-lf-20">
-        <nuxt-link :to="{name:'index'}">首页</nuxt-link>
+        <nuxt-link :to="{ name: 'index' }">首页</nuxt-link>
       </li>
       <li class="item-menu">
-        <nuxt-link :to="{name:'service'}">API接口</nuxt-link>
+        <nuxt-link :to="{ name: 'service' }">API接口</nuxt-link>
       </li>
       <li class="item-menu">
-        <nuxt-link :to="{name:'scene'}">场景化服务</nuxt-link>
+        <nuxt-link :to="{ name: 'scene' }">场景化服务</nuxt-link>
       </li>
     </ul>
     <ul class="header-con-right flex-r">
@@ -32,12 +36,11 @@
           <i slot="prefix" class="el-input__icon el-icon-search"></i>
         </el-input>
       </li>
-      <li class="item-menu header-service-center">
-        <a href="https://service.easyapi.com">服务中心</a>
-      </li>
       <li class="item-menu current-team-box" v-show="token">
-        <a id="showTeamInfo" class="flex-r" :class="{active:showTeamInfo}">
-          <span class="team-icon"></span>
+        <a id="showTeamInfo" class="flex-r" :class="{ active: showTeamInfo }">
+          {{ teamName }}
+          <i v-if="showTeamInfo" class="team-icon icon-arrow-top iconfont"></i>
+          <i v-else class="team-icon icon-xiangxiajiantou iconfont"></i>
         </a>
         <TeamDialog
           @on-creadTeam="jumpPage"
@@ -48,16 +51,26 @@
           :teamList="teamList.content"
         ></TeamDialog>
       </li>
+      <li class="item-menu header-service-center">
+        <a href="https://service.easyapi.com">服务中心</a>
+      </li>
       <li class="item-menu header-login" v-show="token">
         <div class="userAvatar ea-Dropdown">
           <a class="flex-r">
-            <img id="showPersonage" :src="photo+'!icon.jpg'" alt v-if="photo"/>
+            <img
+              id="showPersonage"
+              :src="photo + '!icon.jpg'"
+              alt
+              v-if="photo"
+            />
           </a>
         </div>
-        <div :class="{active:isActive}" class="ea-DropdownMenu">
+        <div :class="{ active: isActive }" class="ea-DropdownMenu">
           <a href="https://team.easyapi.com/notification/">我的通知</a>
           <a href="https://team.easyapi.com/user/edit">个人设置</a>
-          <a @click="quitLogin()" href="https://account.easyapi.com/logout">退出</a>
+          <a @click="quitLogin()" href="https://account.easyapi.com/logout"
+            >退出</a
+          >
         </div>
       </li>
       <li class="item-menu header-login" v-show="!token">
@@ -71,259 +84,257 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  import TeamDialog from './setting/TeamDialog'
-  import Cookies from 'js-cookie'
+import '../assets/iconfont/icon-arrow.css'
+import { mapGetters } from 'vuex'
+import TeamDialog from './setting/TeamDialog'
+import Cookies from 'js-cookie'
 
-  export default {
-    name: 'Header',
-    layout: '',
-    props: ['callback'],
-    components: {
-      TeamDialog
-    },
-    data () {
-      return {
-        token: Cookies.get('authenticationToken'),
-        name: '',
-        isActive: false,
-        showTeamInfo: false
-      }
-    },
-    computed: {
-      ...mapGetters(['photo', 'team', 'teamName', 'teamImg', 'teamList'])
-    },
-    methods: {
-      search () {
-        let _this = this
-        this.$store.commit('SET_SERVICE_NAME', this.name)
-        let obj = {}
-        let name = this.name
-        let type = this.$route.query.type
-        let payType = this.$route.query.payType
-        let sort = this.$route.query.sort
-        if (name) {
-          obj.name = name
-        }
-        if (type) {
-          obj.serviceTypeId = type
-        }
-        if (payType) {
-          obj.type = payType
-        }
-        if (sort) {
-          obj.sort = sort
-        }
-        _this.$router.push({ path: '/service', query: obj })
-      },
-
-      //退出登录
-      quitLogin () {
-        this.$store.dispatch('logout')
-        window.location.href = 'https://www.easyapi.com/user/login'
-      },
-
-      jumpPage () {
-        window.location.href = 'https://www.easyapi.com/launch'
-      },
-      //切换团队
-      changeTeam (id) {
-        this.$store.dispatch('changeTeam', id)
-      }
-    },
-    created: function () {
-    },
-    mounted () {
-      if (Cookies.get('authenticationToken')) {
-        //获取用户信息
-        this.$store.dispatch('getUser')
-      }
-      if (Cookies.get('authenticationToken')) {
-        //获取团队列表
-        this.$store.dispatch('getTeamList')
-      }
-      this.name = this.$store.state.serviceName
-
-      let body = document.querySelector('body')
-      body.addEventListener(
-        'click',
-        e => {
-          if (
-            e.target.id === 'showTeamInfo' ||
-            e.target.className === 'team-icon'
-          ) {
-            this.isActive = false
-            this.showTeamInfo = !this.showTeamInfo
-          } else if (e.target.id === 'showPersonage') {
-            this.isActive = !this.isActive
-            this.showTeamInfo = false
-          } else {
-            this.showTeamInfo = false
-            this.isActive = false
-          }
-        },
-        false
-      )
+export default {
+  name: 'Header',
+  layout: '',
+  props: ['callback'],
+  components: {
+    TeamDialog,
+  },
+  data() {
+    return {
+      token: Cookies.get('authenticationToken'),
+      name: '',
+      isActive: false,
+      showTeamInfo: false,
     }
-  }
+  },
+  computed: {
+    ...mapGetters(['photo', 'team', 'teamName', 'teamImg', 'teamList']),
+  },
+  methods: {
+    search() {
+      let _this = this
+      this.$store.commit('SET_SERVICE_NAME', this.name)
+      let obj = {}
+      let name = this.name
+      let type = this.$route.query.type
+      let payType = this.$route.query.payType
+      let sort = this.$route.query.sort
+      if (name) {
+        obj.name = name
+      }
+      if (type) {
+        obj.serviceTypeId = type
+      }
+      if (payType) {
+        obj.type = payType
+      }
+      if (sort) {
+        obj.sort = sort
+      }
+      _this.$router.push({ path: '/service', query: obj })
+    },
+
+    //退出登录
+    quitLogin() {
+      this.$store.dispatch('logout')
+      window.location.href = 'https://www.easyapi.com/user/login'
+    },
+
+    jumpPage() {
+      window.location.href = 'https://www.easyapi.com/launch'
+    },
+    //切换团队
+    changeTeam(id) {
+      this.$store.dispatch('changeTeam', id)
+    },
+  },
+  created: function () {},
+  mounted() {
+    if (Cookies.get('authenticationToken')) {
+      //获取用户信息
+      this.$store.dispatch('getUser')
+    }
+    if (Cookies.get('authenticationToken')) {
+      //获取团队列表
+      this.$store.dispatch('getTeamList')
+    }
+    this.name = this.$store.state.serviceName
+
+    let body = document.querySelector('body')
+    body.addEventListener(
+      'click',
+      (e) => {
+        if (
+          e.target.id === 'showTeamInfo' ||
+          e.target.classList[0] === 'team-icon'
+        ) {
+          this.isActive = false
+          this.showTeamInfo = !this.showTeamInfo
+        } else if (e.target.id === 'showPersonage') {
+          this.isActive = !this.isActive
+          this.showTeamInfo = false
+        } else {
+          this.showTeamInfo = false
+          this.isActive = false
+        }
+      },
+      false
+    )
+  },
+}
 </script>
 
 <style scoped lang="scss">
-  .header {
-    font-weight: 700;
-    font-size: 14px;
-    height: 60px;
-    line-height: 60px;
-    background: #18c1d6;
-    margin-bottom: 30px;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 0 0 30px;
+.header {
+  font-weight: 700;
+  font-size: 14px;
+  height: 60px;
+  line-height: 60px;
+  background: #18c1d6;
+  margin-bottom: 30px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 0 0 30px;
 
-    a {
-      color: #fff;
-    }
+  a {
+    color: #fff;
+  }
 
-    .header-con-left {
-      li {
-        height: 100%;
-        list-style: none;
+  .header-con-left {
+    li {
+      height: 100%;
+      list-style: none;
 
-        a {
-          height: 60px;
-        }
-      }
-
-      .header-logo {
-        align-items: center;
-
-        a.logo {
-          align-items: center;
-        }
-
-        .circle {
-          display: inline-block;
-          background: white;
-          width: 6px;
-          height: 6px;
-          border-radius: 50%;
-        }
-      }
-
-      .header-market {
-        font-size: 16px;
-        margin-left: 10px;
-        padding-right: 30px;
-        border-right: 1px solid #0bacc0;
-      }
-
-      .item-menu {
-        a {
-          padding: 0 20px;
-        }
+      a {
+        height: 60px;
       }
     }
 
-    .header-con-right {
-      height: 60px;
+    .header-logo {
       align-items: center;
 
-      li {
-        height: 100%;
-        list-style: none;
-
-        img {
-          width: 35px;
-          height: 35px;
-          border-radius: 50%;
-          cursor: pointer;
-        }
+      a.logo {
+        align-items: center;
       }
 
-      .header-search {
+      .circle {
+        display: inline-block;
+        background: white;
+        width: 6px;
+        height: 6px;
+        border-radius: 50%;
+      }
+    }
+
+    .header-market {
+      font-size: 16px;
+      margin-left: 10px;
+      padding-right: 30px;
+      border-right: 1px solid #0bacc0;
+    }
+
+    .item-menu {
+      a {
         padding: 0 20px;
-
-        .search {
-          width: 200px;
-        }
       }
+    }
+  }
 
-      .item-menu {
-        border-left: 1px solid #0bacc0;
+  .header-con-right {
+    height: 60px;
+    align-items: center;
 
-        a {
-          padding: 0 20px;
-        }
+    li {
+      height: 100%;
+      list-style: none;
+
+      img {
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        cursor: pointer;
       }
+    }
 
-      .current-team-box {
-        & > a {
-          position: relative;
-          height: 60px;
-          padding-top: 12.5px;
-          display: inline-block;
+    .header-search {
+      padding: 0 20px;
 
-          &.active {
-            background-color: #19b7cb;
-          }
-
-          &:hover {
-            background-color: #19b7cb;
-          }
-
-          .team-icon {
-            display: inline-block;
-            width: 35px;
-            height: 35px;
-            background: url('../assets/images/team-icon.png') no-repeat;
-            background-size: cover;
-          }
-        }
+      .search {
+        width: 200px;
       }
+    }
 
-      .header-login {
-        & > .ea-Dropdown > a {
-          height: 60px;
-          padding-top: 12.5px;
-        }
+    .item-menu {
+      border-left: 1px solid #0bacc0;
 
+      a {
+        padding: 0 20px;
+      }
+    }
+
+    .current-team-box {
+      position: relative;
+      & > a {
         position: relative;
+        height: 60px;
+        display: inline-block;
+
+        &.active {
+          background-color: #19b7cb;
+        }
+
+        &:hover {
+          background-color: #19b7cb;
+        }
+
+        .team-icon {
+          // display: inline-block;
+          // width: 35px;
+          // height: 35px;
+          // background: url('../assets/images/team-icon.png') no-repeat;
+          // background-size: cover;
+        }
       }
     }
-  }
 
-  .ea-DropdownMenu {
-    position: absolute;
-    top: 60px;
-    right: 0;
-    z-index: 100;
-    border: 1px solid #eee;
-    border-top: none;
-    box-shadow: 0px 1px 3px #ddd;
-    background-color: #fff;
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
-    width: 100px;
-    display: none;
-
-    &.active {
-      display: block;
-    }
-
-    a {
-      display: block;
-      line-height: 26px;
-      height: inherit;
-      padding-left: 15px;
-      color: #777;
-      font-weight: normal;
-
-      &:hover {
-        background-color: #1ac1d6;
-        color: #fff;
+    .header-login {
+      & > .ea-Dropdown > a {
+        height: 60px;
+        padding-top: 12.5px;
       }
+
+      position: relative;
     }
   }
+}
+
+.ea-DropdownMenu {
+  position: absolute;
+  top: 60px;
+  right: 0;
+  z-index: 100;
+  border: 1px solid #eee;
+  border-top: none;
+  box-shadow: 0px 1px 3px #ddd;
+  background-color: #fff;
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
+  width: 100px;
+  display: none;
+
+  &.active {
+    display: block;
+  }
+
+  a {
+    display: block;
+    line-height: 26px;
+    height: inherit;
+    padding-left: 15px;
+    color: #777;
+    font-weight: normal;
+
+    &:hover {
+      background-color: #1ac1d6;
+      color: #fff;
+    }
+  }
+}
 </style>
-
-
--- components/setting/TeamDialog.vue
+++ components/setting/TeamDialog.vue
@@ -6,7 +6,6 @@
       <a href="https://gateway.easyapi.com/" class="ea-link">网关</a>
       <a href="https://service.easyapi.com/" class="ea-link">服务</a>
     </div>
-    <h2 class="current-team-name lrPading-20">当前团队: {{ teamName }}</h2>
     <div class="clear current-team-content lrPading-20">
       <img
         class="lf teams-img"
@@ -15,6 +14,7 @@
         v-if="teamImg"
       />
       <div class="lf teams-img-r">
+        <p>{{ teamName }}</p>
         <div class="team-btn">
           <a class="ea-btn" href="https://team.easyapi.com/account">账户</a>
           <a class="ea-btn" href="https://team.easyapi.com/members">成员</a>
@@ -25,7 +25,9 @@
     <div class="change-team-box">
       <h2 class="lrPading-20">
         切换团队：
-        <el-button type="primary" size="small" @click="jumpPage">创建新团队</el-button>
+        <el-button type="primary" size="small" @click="jumpPage"
+          >创建新团队</el-button
+        >
       </h2>
       <div class="ea-team-list-box lrPading-20">
         <a
@@ -47,196 +49,193 @@
   </div>
 </template>
 <script>
-  export default {
-    name: '',
-    components: {},
-    props: {
-      showTeamDialog: {
-        type: Boolean,
-        default: false
+export default {
+  name: '',
+  components: {},
+  props: {
+    showTeamDialog: {
+      type: Boolean,
+      default: false,
+    },
+    teamImg: String,
+    teamName: String,
+    teamList: {
+      type: Array,
+      default: function () {
+        return []
       },
-      teamImg: String,
-      teamName: String,
-      teamList: {
-        type: Array,
-        default: function() {
-          return []
-        }
-      }
     },
-    data () {
-      return {
-        showTeamInfo: this.showTeamDialog
-      }
-    },
-    //计算属性
-    computed: {},
-    watch: {
-      showTeamDialog: function(v) {
-        return (this.showTeamInfo = v)
-      }
-    },
-    created () {
-    },
-    mounted () {
-    },
-    //keep-alive 组件激活时调用
-    activated () {
-    },
-    //keep-alive 组件停用时调用。
-    deactivated () {
-    },
-    //方法
-    methods: {
-      tabTeamFn (e) {
-        this.$emit('on-selectTeam', e)
-      },
-      jumpPage () {
-        this.$emit('on-creadTeam')
-      }
+  },
+  data() {
+    return {
+      showTeamInfo: this.showTeamDialog,
     }
-  }
+  },
+  //计算属性
+  computed: {},
+  watch: {
+    showTeamDialog: function (v) {
+      return (this.showTeamInfo = v)
+    },
+  },
+  created() {},
+  mounted() {},
+  //keep-alive 组件激活时调用
+  activated() {},
+  //keep-alive 组件停用时调用。
+  deactivated() {},
+  //方法
+  methods: {
+    tabTeamFn(e) {
+      this.$emit('on-selectTeam', e)
+    },
+    jumpPage() {
+      this.$emit('on-creadTeam')
+    },
+  },
+}
 </script>
 <style lang="scss" scoped>
-  .current-team-info {
-    max-height: 700px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    position: absolute;
+.current-team-info {
+  max-height: 700px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  position: absolute;
+  padding: 0 20px;
+  top: 60px;
+  right: 0px;
+  background-color: #ffffff;
+  box-shadow: 0px 1px 3px #ddd;
+  border: 1px solid #eee;
+  border-top: none;
+  width: 410px;
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
+  display: none;
+  z-index: 9999;
+
+  .lrPading-20 {
     padding: 0 20px;
-    top: 50px;
-    right: 75px;
-    background-color: #ffffff;
-    box-shadow: 0px 1px 3px #ddd;
-    border: 1px solid #eee;
-    border-top: none;
-    width: 410px;
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
-    display: none;
-    z-index: 9999;
+  }
 
-    .current-team-name {
-      height: 60px;
-      line-height: 60px;
-      font-weight: bold;
-      border-bottom: 1px solid #eaeaea;
-      font-size: 18px;
-    }
-    .current-team-title {
+  .current-team-title {
+    height: 50px;
+    line-height: 50px;
+    padding: 0 20px;
+    border-bottom: 1px solid #eaeaea;
+    display: flex;
+    justify-content: space-between;
+    .ea-link {
       height: 50px;
-      line-height: 50px;
-      padding: 0 20px;
-      border-bottom: 1px solid #eaeaea;
-      display: flex;
-      justify-content: space-between;
-      .ea-link {
-        height: 50px;
-        display: inline-block;
+      display: inline-block;
+      color: #333;
+    }
+  }
+
+  .current-team-content {
+    border-bottom: 1px solid #eaeaea;
+    height: 110px;
+    padding: 10px 0;
+    display: flex;
+
+    .teams-img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      margin-top: 5px;
+    }
+
+    .teams-img-r {
+      display: inline-block;
+      height: 99px;
+      vertical-align: top;
+      padding-left: 20px;
+
+      & > p {
         color: #333;
-      }
-    }
-
-    .current-team-content {
-      border-bottom: 1px solid #eaeaea;
-      height: 110px;
-      padding: 10px 0;
-      display: flex;
-
-      .teams-img {
-        width: 80px;
-        height: 80px;
-        border-radius: 50%;
-        margin-top: (10 / 2) px;
+        height: auto;
+        line-height: 16px;
+        font-size: 16px;
+        padding-top: 10px;
       }
 
-      .teams-img-r {
-        display: inline-block;
-        height: 99px;
-        vertical-align: top;
-        padding-left: 20px;
-
-        & > p {
-          color: #333;
-          height: auto;
-          line-height: 16px;
-          font-size: 16px;
-          padding-top: 10px;
-        }
-
-        .team-btn {
-          height: 50px;
-          line-height: 50px;
-
-          .ea-btn {
-            line-height: 0;
-            padding: 16px 15px;
-            border: 1px solid #ddd;
-            color: #333;
-            border-radius: 5px;
-            box-sizing: content-box;
-            margin-right: 5px;
-            font-weight: normal;
-            font-size: 14px;
-          }
-        }
-      }
-    }
-
-    .change-team-box {
-      // border-bottom: 1px solid #eaeaea;
-
-      & > h2 {
+      .team-btn {
         height: 50px;
         line-height: 50px;
-        font-weight: bold;
-        font-size: 20px;
-      }
+        margin-top: 10px;
 
-      .ea-team-list-box {
-        /* 父元素设置弹性布局 */
-        display: flex;
-        /* 主轴方向 */
-        /* column 列上下 */
-        flex-direction: row;
-        /* 是否换行 */
-        flex-wrap: wrap;
-        /* 在主轴对齐方式 */
-        justify-content: flex-start;
-
-        .ea-team-item {
-          width: 50%;
+        .ea-btn {
+          line-height: 0;
+          padding: 14px 15px;
+          border: 1px solid #ddd;
           color: #333;
-          font-size: 14px;
+          border-radius: 5px;
+          box-sizing: content-box;
+          margin-right: 5px;
           font-weight: normal;
-          height: auto;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-
-          & > img {
-            width: 28px;
-            height: 28px;
-            border-radius: 50%;
-            vertical-align: middle;
-            margin: 0;
-          }
-
-          & > span {
-            display: inline-block;
-            width: calc(100% - 28px);
-            padding: 0 10px;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            box-sizing: border-box;
-          }
+          font-size: 14px;
         }
       }
     }
   }
 
-  .current-team-info.active {
-    display: block;
+  .change-team-box {
+    // border-bottom: 1px solid #eaeaea;
+    .el-button {
+      background: #2db7f5;
+    }
+
+    & > h2 {
+      height: 50px;
+      line-height: 50px;
+      font-weight: bold;
+      font-size: 16px;
+    }
+
+    .ea-team-list-box {
+      /* 父元素设置弹性布局 */
+      display: flex;
+      /* 主轴方向 */
+      /* column 列上下 */
+      flex-direction: row;
+      /* 是否换行 */
+      flex-wrap: wrap;
+      /* 在主轴对齐方式 */
+      justify-content: flex-start;
+
+      .ea-team-item {
+        width: 50%;
+        color: #333;
+        font-size: 14px;
+        font-weight: normal;
+        height: auto;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        & > img {
+          width: 28px;
+          height: 28px;
+          border-radius: 50%;
+          vertical-align: middle;
+          margin: 0;
+        }
+
+        & > span {
+          display: inline-block;
+          width: calc(100% - 28px);
+          padding: 0 10px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          box-sizing: border-box;
+        }
+      }
+    }
   }
+}
+
+.current-team-info.active {
+  display: block;
+}
 </style>
