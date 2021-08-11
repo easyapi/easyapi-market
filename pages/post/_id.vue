8c5dab4f0bd63bc71dcc8ba0c4d8f483ef20f9d1
leida <zhang20084@126.com>
2021-08-11 11:27:59 +0800

    fix:文章详情改成支持SEO

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <Header></Header>
-    <div class="container">
+    <div class='container'>
       <div class='detail-wrapper'>
         <div class='detail-title'>
-          <span>{{article && article.title}}</span>
-          <label class="time">{{article && article.updateTime.split(' ')[0]}}</label>
+          <span>{{ article.title }}</span>
+          <label class='time'>{{ article.updateTime.split(' ')[0] }}</label>
         </div>
-        <div class='detail-content' v-html='article && article.content'>
+        <div class='detail-content' v-html='article.content'>
         </div>
       </div>
     </div>
@@ -16,106 +16,103 @@
 </template>
 
 <script>
-  import { getArticle } from '../../api/article'
-  import Header from '~/components/header'
-  import Footer from '~/components/footer'
+import { getArticle } from '../../api/article'
+import Header from '~/components/header'
+import Footer from '~/components/footer'
+import { getServiceList } from '../../api/service'
 
-  export default {
-    name: 'post-detail',
-    head () {
-      return {
-        title: this.article && this.article.title + ' - EasyAPI服务市场',
-        meta: [
-          { hid: 'description', name: 'description', content: this.article.title },
-          { hid: 'keyword', name: 'keyword', content: '文章详情' }
-        ]
-      }
-    },
-    components: {
-      Header,
-      Footer
-    },
-    data () {
-      return {
-        article: null
-      }
-    },
-    methods: {
-      getArticle () {
-        getArticle(this.$route.params.id,this).then(res => {
-          this.article = res.data.content
-        })
-      }
-    },
-    created () {
-    },
-    mounted () {
-      this.getArticle()
+export default {
+  name: 'post-detail',
+  head() {
+    return {
+      title: this.article.title + ' - EasyAPI服务市场',
+      meta: [
+        { hid: 'description', name: 'description', content: this.article.title },
+        { hid: 'keyword', name: 'keyword', content: '文章详情' }
+      ]
+    }
+  },
+  components: {
+    Header,
+    Footer
+  },
+  data() {
+    return {
+      article: null
+    }
+  },
+  async asyncData(context) {
+    let [res] = await Promise.all([
+      getArticle(context.route.params.id, context)
+    ])
+    return {
+      article: res.data.content
     }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-  .detail-wrapper {
-    padding: 0 58px;
-    border: 1px solid #ddd;
-    font-size: 14px;
-    line-height: 24px;
-    color: #333;
-  }
+<style lang='scss' scoped>
+.detail-wrapper {
+  padding: 0 58px;
+  border: 1px solid #ddd;
+  font-size: 14px;
+  line-height: 24px;
+  color: #333;
+}
 
-  .detail-title {
-    padding-bottom: 30px;
-    font-size: 14px;
-    color: #a9a9a9;
-    text-align: center;
-    padding-top: 32px;
-    border-bottom: 1px solid #ddd;
-  }
+.detail-title {
+  padding-bottom: 30px;
+  font-size: 14px;
+  color: #a9a9a9;
+  text-align: center;
+  padding-top: 32px;
+  border-bottom: 1px solid #ddd;
+}
 
-  .detail-title span {
-    display: block;
-    color: #333;
-    text-align: center;
-    font-size: 18px;
-    font-weight: bolder;
-  }
+.detail-title span {
+  display: block;
+  color: #333;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bolder;
+}
 
-  .time {
-    margin-top: 5px;
-    display: block;
+.time {
+  margin-top: 5px;
+  display: block;
 
-  }
+}
 
-  .detail-content {
-    padding-top: 30px;
-  }
+.detail-content {
+  padding-top: 30px;
+}
 
-  .detail-con-inner {
-    margin-bottom: 30px;
-    font-size: 14px;
-    line-height: 24px;
-    color: #333;
-  }
+.detail-con-inner {
+  margin-bottom: 30px;
+  font-size: 14px;
+  line-height: 24px;
+  color: #333;
+}
 
-  .detail-con-inner span {
+.detail-con-inner span {
 
-  }
+}
 
-  .detail-con-inner div {
-    margin-top: 35px;
-    text-align: center;
-  }
+.detail-con-inner div {
+  margin-top: 35px;
+  text-align: center;
+}
 
-  .detail-wrapper p {
-    margin-bottom: 10px;
-  }
+.detail-wrapper p {
+  margin-bottom: 10px;
+}
 
-  .detail-wrapper img {
-    display: block;
-    max-width: 100%;
-    margin: 20px auto;
-    padding: 5px;
-    border: 1px solid #ddd;
-  }
+.detail-wrapper img {
+  display: block;
+  max-width: 100%;
+  margin: 20px auto;
+  padding: 5px;
+  border: 1px solid #ddd;
+}
 </style>
