89a9df7a93864b7367cd9ab076bd5236875bd63f
chenyun123 <1537299563@qq.com>
2021-08-27 18:30:42 +0800

    点击管理弹窗,调用接口

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <Header></Header>
-    <div class='container'>
-      <div v-if='!noData'>
-        <div v-for='item of list' :key='item.articleId'>
-          <ListItem v-bind:list='item'></ListItem>
+    <div class="container">
+      <div v-if="!noData">
+        <div v-for="item of list" :key="item.articleId">
+          <ListItem v-bind:list="item"></ListItem>
         </div>
       </div>
-      <div v-else class='no-data'>
-        <el-empty description='暂无数据'></el-empty>
+      <div v-else class="no-data">
+        <el-empty description="暂无数据"></el-empty>
       </div>
-      <div class='loading' v-if='loading'>
-        <Icon type='ios-loading' size='18' class='demo-spin-icon-load'></Icon>
+      <div class="loading" v-if="loading">
+        <Icon type="ios-loading" size="18" class="demo-spin-icon-load"></Icon>
         <div>加载中......</div>
       </div>
-      <div class='noMoreData' v-if='noMoreData'>
+      <div class="noMoreData" v-if="noMoreData">
         <div>没有更多数据了...</div>
       </div>
     </div>
@@ -35,14 +35,14 @@ export default {
       title: '市场动态 - EasyAPI服务市场',
       meta: [
         { hid: 'description', name: 'description', content: '市场动态' },
-        { hid: 'keyword', name: 'keyword', content: '市场动态' }
-      ]
+        { hid: 'keyword', name: 'keyword', content: '市场动态' },
+      ],
     }
   },
   components: {
     Header,
     Footer,
-    ListItem
+    ListItem,
   },
   data() {
     return {
@@ -53,34 +53,33 @@ export default {
       pagination: {
         size: 15,
         page: 0,
-        totalPages: 0
-      }
+        totalPages: 0,
+      },
     }
   },
   methods: {
     getPageList() {
-      if (this.pagination.page < this.pagination.totalPages - 1) {
-        this.pagination.page = this.pagination.page + 1
-        this.getArticleList()
-      }
-      if (this.pagination.page === this.pagination.totalPages - 1) {
+      this.pagination.page = this.pagination.page + 1
+      if (this.pagination.page == this.pagination.totalPages) {
         this.noMoreData = true
+      }
+      if (this.pagination.page < this.pagination.totalPages) {
+        this.getArticleList()
       }
     },
     getArticleList() {
       this.loading = true
       let params = {
         size: this.pagination.size,
-        page: this.pagination.page
+        page: this.pagination.page,
       }
       getArticleList(params, this).then((res) => {
+        this.loading = false
         if (res.data.code === 1) {
           this.list = this.list.concat(res.data.content)
           this.pagination.totalPages = res.data.totalPages
-          this.loading = false
         } else {
           this.noData = true
-          this.loading = false
         }
       })
     },
@@ -93,20 +92,21 @@ export default {
       if (scrollHeight - clientHeight - scrollTop <= 150) {
         // 滚动到底部，逻辑代码
         //事件处理
-        this.getPageList()
+        if (!this.loading) {
+          this.getPageList()
+        }
       }
-    }
+    },
   },
-  created() {
-  },
+  created() {},
   mounted() {
     window.addEventListener('scroll', this.lazyLoading)
     this.getArticleList()
-  }
+  },
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .loading {
   text-align: center;
   color: #2d8cf0;
