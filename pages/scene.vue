979716c9e9cae6dddecd46defc846b9c081c3276
chenyun123 <1537299563@qq.com>
2021-11-24 13:36:17 +0800

    场景化服务优化

--- pages/scene.vue
+++ pages/scene.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <Header></Header>
-    <div class='main'>
+    <div class="main">
       <DivListWarp>
-        <div class='scene-card' v-for='(item,index) in imgList' :key='index'>
-          <a :href='item.href' class='img' target='_blank'>
+        <div class="scene-card" v-for="(item, index) in imgList" :key="index">
+          <a :href="item.href" class="img" target="_blank">
             <div>
-              <img :src='item.src' alt='图片加载中...'>
+              <img :src="item.src" alt="图片加载中..." />
             </div>
             <span>{{ item.title }}</span>
           </a>
@@ -27,7 +27,7 @@ export default {
   components: {
     Header,
     DivListWarp,
-    Footer
+    Footer,
   },
   data() {
     return {
@@ -35,64 +35,68 @@ export default {
         {
           href: 'https://scrm.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/scrm.png',
-          title: '会员营销'
+          title: '会员营销',
         },
         {
           href: 'https://withdraw.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/withdraw.png',
-          title: '快速提现'
+          title: '快速提现',
         },
         {
           href: 'https://ad.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/ad.png',
-          title: '广告管家'
+          title: '广告管家',
         },
         {
           href: 'https://clockin.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/clockin.png',
-          title: '签到打卡'
+          title: '签到打卡',
         },
         {
           href: 'https://shop.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/shop.png',
-          title: '微商城'
+          title: '微商城',
         },
         {
           href: 'https://jifen.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/jifen.png',
-          title: '积分商城'
+          title: '积分商城',
         },
         {
           href: 'https://fapiao.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/invoice.png',
-          title: '发票管理'
+          title: '发票管理',
         },
         {
           href: 'https://workorder.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/work_order.png',
-          title: '工单系统'
+          title: '工单系统',
         },
         {
           href: 'https://lottery.easyapi.com',
           src: 'https://qiniu.easyapi.com/scene/lottery.png',
-          title: '抽奖活动'
-        }
-      ]
+          title: '抽奖活动',
+        },
+      ],
     }
   },
   head() {
     return {
       title: '场景化服务 - EasyAPI服务市场',
       meta: [
-        { hid: 'description', name: 'description', content: '服务市场场景化服务' },
-        { hid: 'keyword', name: 'keyword', content: '服务市场场景化服务' }
-      ]
+        {
+          hid: 'description',
+          name: 'description',
+          content: '服务市场场景化服务',
+        },
+        { hid: 'keyword', name: 'keyword', content: '服务市场场景化服务' },
+      ],
     }
-  }
+  },
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .scene-card {
   text-align: center;
   margin-bottom: 20px;
@@ -101,18 +105,19 @@ export default {
   border: 1px solid #ddd;
 
   img {
-    display: block;
-    width: 387px;
+    width: 386px;
     height: 223px;
+    vertical-align: middle;
+    display: flex;
   }
 
   span {
     display: block;
+    width: 387px;
     text-align: center;
     line-height: 50px;
     color: #000000;
     font-size: 18px;
   }
 }
-
 </style>
